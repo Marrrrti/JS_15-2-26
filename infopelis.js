38926b0729916d5b157822f104918c74b54c2c92
Marrrrti <2526_marti.fernandez@iticbcn.cat>
2026-02-15 23:42:57 +0100

    Add files via upload

---
+++ infopelis.js
@@ -0,0 +1,170 @@
+const pelicules = [
+   {
+    title: "The Godfather",
+    year: 1972,
+    genre: "Drama",
+    director: "Francis Ford Coppola",
+    actors: "Marlon Brando, Al Pacino",
+    oscars: 3
+  },
+   {
+    title: "Pulp Fiction",
+    year: 1994,
+    genre: "Crime",
+    director: "Quentin Tarantino",
+    actors: "John Travolta, Uma Thurman",
+    oscars: 10
+  },
+   {
+    title: "Shindler's List",
+    year: 1993,
+    genre: "Drama",
+    director: "Steven Spielberg",
+    actors: "Liam Neeson, Ralph Fiennes",
+    oscars: 7
+  },
+   {
+    title: "The Dark Knight",
+    year: 2008,
+    genre: "Action",
+    director: "Christopher Nolan",
+    actors: "Christian Bale, Heath Ledger",
+    oscars: 2
+  },
+  {
+    title: "Titanic",
+    year: 1997,
+    genre: "Romance",
+    director: "James Cameron",
+    actors: "Leonardo DiCaprio, Kate Winslet",
+    oscars: 11
+  },
+   {
+    title: "Forrest Gump",
+    year: 1994,
+    genre: "Drama",
+    director: "Robert Zemeckis",
+    actors: "Tom Hanks, Robin Wright",
+    oscars: 6
+  },
+   {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+    genre: "Fantasy",
+    director: "Peter Jackson",
+    actors: "Elijah Wood, Ian McKellen",
+    oscars: 11
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genre: "Science Fiction",
+    director: "Christopher Nolan",
+    actors: "Leonardo DiCaprio",
+    oscars: 4
+  },
+   {
+    title: "Gladiator",
+    year: 2000,
+    genre: "Action",
+    director: "Ridley Scott",
+    actors: "Russell Crowe, Joaquin Phoenix",
+    oscars: 5
+  },
+  {
+    title: "The Departed",
+    year: 2006,
+    genre: "Crime",
+    director: "Martin Scorsese",
+    actors: "Leonardo DiCaprio, Matt Damon",
+    oscars: 2
+  },
+  {
+    title: "Schindler's List",
+    year: 1993,
+    genre: "Drama", 
+    director: "Steven Spielberg",
+    actors: "Liam Neeson, Ralph Fiennes",
+    oscars: 7
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    genre: "Drama",
+    director: "Robert Zemeckis",
+    actors: "Tom Hanks, Robin Wright",
+    oscars: 6
+  },
+  {
+    title: "12 Years a Slave",
+    year: 2013,
+    genre: "Drama",
+    director: "Steve McQueen",
+    actors: "Chiwetel Ejiofor, Michael Fassbender",
+    oscars: 4
+  },
+  {
+    title: "American Beauty",
+    year: 1999,
+    genre: "Drama",
+    director: "Sam Mendes",
+    actors: "Kevin Spacey, Annette Bening",
+    oscars: 5
+  },
+  {
+    title: "Slumdog Millionaire",
+    year: 2008,
+    genre: "Drama",
+    director: "Danny Boyle",
+    actors: "Dev Patel, Freida Pinto",
+    oscars: 8
+  },
+  {
+    title: "The Silence of the Lambs",
+    year: 1991,
+    genre: "Thriller",
+    director: "Jonathan Demme",
+    actors: "Jodie Foster, Anthony Hopkins",
+    oscars: 5
+  },
+  {
+    title: "The Green Mile",
+    year: 1999,
+    genre: "Fantasy",
+    director: "Frank Darabont",
+    actors: "Tom Hanks, Michael Clarke Duncan",
+    oscars: 1 
+  },
+  { 
+    title: "The Prestige",
+    year: 2006,
+    genre: "Thriller", 
+    director: "Christopher Nolan",
+    actors: "Hugh Jackman, Christian Bale",
+    oscars: 0
+  },
+  {
+    title: "The Lion King",
+    year: 1994,
+    genre: "Animation",
+    director: "Roger Allers, Rob Minkoff",
+    actors: "Matthew Broderick, Jeremy Irons",
+    oscars: 2 
+  },
+  { 
+    title: "The Matrix",
+    year: 1999,
+    genre: "Science Fiction",
+    director: "The Wachowskis",
+    actors: "Keanu Reeves, Laurence Fishburne",
+    oscars: 4
+  },
+  {
+    title: "The Avengers",
+    year: 2012,
+    genre: "Action",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr., Chris Evans",
+    oscars: 0
+  }
+];
